c44caaef911438ccf23c859301a2039f551172ec
Braden Schneider <60082961+hw2007@users.noreply.github.com>
2026-05-19 09:09:10 -0600

    Add CLI arg to only save the final state of the system

--- examples/fsrk_embedded_error/rk4_test/integrate.py
+++ examples/fsrk_embedded_error/rk4_test/integrate.py
@@ -1,15 +1,28 @@
 # Equation: dy/dt = y
 
+# Pass --result-only to only save the final result (not each step in time)
+
 import fractional_step as fs
 import math
 import time
+import sys
+
+args = sys.argv[1:]
+if "--result-only" in args:
+    fname = None
+    result_only = True
+    print("Only result will be saved!")
+else:
+    fname = "results.csv"
+    result_only = False
+
 
 # Start & end time
 t0 = 0
 tf = 10
 
 # Timestep size
-dt = 1/1000
+dt = 1/100000
 
 # Initial condition
 y0 = 10 
@@ -29,6 +42,11 @@ print("BEGINNING SOLVE!")
 start_time = time.perf_counter()
 
 result = fs.fractional_step(operators, dt, y0, t0, tf, "Strang", methods, fname="results.csv")
+
+if result_only:
+    file = open("results.csv", "w")
+    file.write(str(result))
+    file.close()
 
 end_time = time.perf_counter()
 print(f"DONE! Solved in {end_time - start_time} seconds.")
