8790e38338bc5f8bb850281f8735d5a5d8059cce
Braden Schneider <60082961+hw2007@users.noreply.github.com>
2026-05-27 15:42:27 -0600

    Add portero method

--- examples/fsrk_embedded_error/methods.py
+++ examples/fsrk_embedded_error/methods.py
@@ -32,3 +32,19 @@ b_sd2 = np.array([1 - GAMMA, GAMMA])
 b_be = np.array([1, 0])
 
 sd2_be = bt.EmbeddedTableau(c=c_sd2, a=A_sd2, b=b_sd2, b_aux=b_be, order=1)
+
+# 2(1) Embedded pair from Portero 2012 paper
+
+c = np.array([1, 1, 4/9, 1/3])
+
+A = np.array([
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [-343/180, 5/9, 47/20, 0],
+    [-1592/300, -121/60, 564/100, 47/20]
+])
+
+b_main = [1/10, 1/4, 9/10, 3/4]
+b_embedded = [1, 1, 0, 0]
+
+portero_2_1 = bt.EmbeddedTableau(c=c, a=A, b=b_main, b_aux=b_embedded, order=1)
